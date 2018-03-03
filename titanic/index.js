cc9f4d9866037275b7c7ddf44f9851aab98114cd
Jacob Koperski <wijakoperski@gmail.com>
2018-03-02 21:37:46 -0600

    Modified titanic reading to use filesystem node module

--- titanic/index.js
+++ titanic/index.js
@@ -1,17 +1,6 @@
 "use strict";
 
-var $, jQuery;
-$ = jQuery = require("jquery");
-const parser = require("../lib/jquery-csv.min.js");
-const FileApi = require('file-api');
+const Parser = require("../lib/jquery-csv.min.js");
+const fs = require("fs");
 
-const reader = new FileApi.FileReader();
-reader.readAsDataURL(new FileApi.File("./data/titanic/train.csv"));
-
-reader.on("data", (data) => {
-  console.log(parser.toObjects(data.toString()));
-});
-
-// reader.addEventListener('load', (event) => {
-//   console.log("dataUrlSize:", event.target.result.length);
-// });
+console.log(Parser.toObjects(fs.readFileSync("./data/titanic/train.csv", { encoding: 'utf-8' })));
